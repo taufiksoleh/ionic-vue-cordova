76dd99780abb691febd4bac626c048d1ea00d5aa
Taufik Soleh <taufikhope@gmail.com>
2020-09-24 02:53:12 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Ionic from '@ionic/vue';
+
+
 Vue.config.productionTip = false
+Vue.use(Ionic);
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
